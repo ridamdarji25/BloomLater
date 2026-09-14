b5d557fe85311ab9b9e0330a57ffbed82fc8a3ca
ridamdarji25 <ridamdarji2729@gmail.com>
2026-09-14 12:32:48 +0530

    ci(gitops): add GitOps stage with loop prevention and credential-safe push

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -35,12 +35,12 @@ variable "cluster_version" {
 
 variable "node_instance_type" {
   type    = string
-  default = "t3a.medium"
+  default = "t3.medium"
 }
 
 variable "node_desired_size" {
   type    = number
-  default = 1
+  default = 2
 }
 
 variable "node_min_size" {
